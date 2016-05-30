d313ea7c3b09b761aad43ad72f9d3b92ef4acda9
Naeem Akram <naeem.akram@aspose.com>
2016-05-30 11:52:35 +0500

    Update RunExamples.vb

--- Examples/VisualBasic/RunExamples.vb
+++ Examples/VisualBasic/RunExamples.vb
@@ -9,7 +9,7 @@ Imports VisualBasic.Working_with_Objects
 
 Module RunExamples
     Sub Main()
-        Console.WriteLine("Open RunExamples.cs. In Main() method, Un-comment the example that you want to run")
+        Console.WriteLine("Open RunExamples.vb. In Main() method, Un-comment the example that you want to run")
         Console.WriteLine("=====================================================")
 
         ' Un-comment the one you want to try out
