d179b10782d94a34f2c3ef7d20c236d764f41419
Naeem Akram <naeem.akram@aspose.com>
2020-04-15 19:01:07 +0500

    Removed NPGSql reference

--- LiveDemos/src/Aspose.3D.Live.Demos.UI/App_Start/BundleConfig.cs
+++ LiveDemos/src/Aspose.3D.Live.Demos.UI/App_Start/BundleConfig.cs
@@ -12,22 +12,22 @@ namespace Aspose.ThreeD.Live.Demos.UI
 		// For more information on Bundling, visit https://go.microsoft.com/fwlink/?LinkID=303951
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
-							"~/js/WebForms/WebForms.js",
-							"~/js/WebForms/WebUIValidation.js",
-							"~/js/WebForms/MenuStandards.js",
-							"~/js/WebForms/Focus.js",
-							"~/js/WebForms/GridView.js",
-							"~/js/WebForms/DetailsView.js",
-							"~/js/WebForms/TreeView.js",
-							"~/js/WebForms/WebParts.js"));
+			//bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
+			//				"~/js/WebForms/WebForms.js",
+			//				"~/js/WebForms/WebUIValidation.js",
+			//				"~/js/WebForms/MenuStandards.js",
+			//				"~/js/WebForms/Focus.js",
+			//				"~/js/WebForms/GridView.js",
+			//				"~/js/WebForms/DetailsView.js",
+			//				"~/js/WebForms/TreeView.js",
+			//				"~/js/WebForms/WebParts.js"));
 
-			// Order is very important for these files to work, they have explicit dependencies
-			bundles.Add(new ScriptBundle("~/bundles/MsAjaxJs").Include(
-					"~/js/WebForms/MsAjax/MicrosoftAjax.js",
-					"~/js/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
-					"~/js/WebForms/MsAjax/MicrosoftAjaxTimer.js",
-					"~/js/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
+			//// Order is very important for these files to work, they have explicit dependencies
+			//bundles.Add(new ScriptBundle("~/bundles/MsAjaxJs").Include(
+			//		"~/js/WebForms/MsAjax/MicrosoftAjax.js",
+			//		"~/js/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+			//		"~/js/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+			//		"~/js/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
 
 			// Use the Development version of Modernizr to develop with and learn from. Then, when you’re
 			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need
@@ -52,17 +52,17 @@ namespace Aspose.ThreeD.Live.Demos.UI
 				});
 
 
-			bundles.Add(
-				new ScriptBundle("~/bundles/AsposeShared")
-				.Include(					
-					"~/Scripts/Shared/Alert.js",
-					"~/Scripts/Shared/Resources.js",
-					"~/Scripts/Shared/DownloadResult.js",
-					"~/Scripts/Shared/Loader.js",
-					"~/Scripts/Shared/UploadFile.js",
-					"~/Scripts/Shared/Work.js"
-				)
-			);
+			//bundles.Add(
+			//	new ScriptBundle("~/bundles/AsposeShared")
+			//	.Include(					
+			//		"~/Scripts/Shared/Alert.js",
+			//		"~/Scripts/Shared/Resources.js",
+			//		"~/Scripts/Shared/DownloadResult.js",
+			//		"~/Scripts/Shared/Loader.js",
+			//		"~/Scripts/Shared/UploadFile.js",
+			//		"~/Scripts/Shared/Work.js"
+			//	)
+			//);
 		}
 	}
 }
